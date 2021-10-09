1c224570a9bb18a6c0939e196371582e74a4659d
leblabac <87709841+leblabac@users.noreply.github.com>
2021-10-09 17:30:39 -0500

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -1,6 +1,12 @@
 # The data we need to retrieve:
+
 # Total number of votes cast
+# The total number of votes cast across three counties was 369,711
+
 # complete list of candidates who received votes
+# The complete list of candidates who received votes in the election include:
+# Charles Casper Stockham, Diana DeGette, Raymon Anthony Doane
+
 # Total number of votes each candidate received
 # Percentage of votes each candidate won
 # The winner of the election based on popular vote
@@ -15,10 +21,32 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Initialize a total vote counter.
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
 # Read and print the header row.
     headers = next(file_reader)
-    print(headers)   
+
+# Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+       
+        # Print the candidate name from each row
+        candidate_name = row[2]
+        
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+        
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+# Print the candidate list.
+print(candidate_options)   
